c426f6ccbc9412160682dd719eaacd9379bb5408
Prathmesh2001 <prathmeshw1102@gmail.com>
2021-11-12 15:48:37 +0530

    pyro4 initial commit

--- ChatInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.RemoteException;
-
-public interface ChatInterface extends java.rmi.Remote {
-    boolean checkClientCredintials(ChatInterface ci,String name, String pass) throws RemoteException;
-    void broadcastMessage(String name,String message) throws RemoteException;
-    void sendMessageToClient(String message) throws RemoteException;
-}
